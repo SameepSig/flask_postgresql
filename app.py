755aafa7e00bbc5ab45a5c1b24ec1de873f2f0f0
SameepSig <sameepsigdel@gmail.com>
2024-05-14 16:30:36 +0545

    venv added in git ignore, postgres database setup with post method

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://sameep:root@localhost:5432/sameep'
+
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(200), nullable=False)
+    done = db.Column(db.Boolean, default=False)
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def index():
-    return 'hi'
+    return jsonify({"key":"hello"})
+
+@app.route('/tasks')
+def get_tasks():
+    tasks = Task.query.all()
+    task_list = [
+        {'id': task.id, 'title': task.title, 'done':task.done} for task in tasks
+    ]
+    return jsonify({"tasks":task_list})
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    data = request.get_json()
+    new_task = Task(title= data['title'], done= data['done'])
+    db.session.add(new_task)
+    db.session.commit()
+    return jsonify({"message": 'Task created'}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
