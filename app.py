df2d2ea8b96913056b9cb873fbf27abc30597d8c
SameepSig <sameepsigdel@gmail.com>
2024-05-27 05:57:48 +0545

    changed database user to postgres from sameep

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://sameep:root@localhost:5432/sameep'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:root@localhost:5432/sameep'
 
 db = SQLAlchemy(app)
 
